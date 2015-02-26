3ddfd4599d03d3254ea8b184565b785de8b0b97b
jhavu042 <jhavu042@newschool.edu>
2015-02-26 01:03:29 +0000

    ok

---
+++ lab/inclass3/application.js
@@ -0,0 +1,20 @@
+ $ ()
+
+//variables
+//var name = 'Urvita';
+//var height = '5.4';
+
+//string
+//boolean  0/1 true/false
+// number
+// array
+
+var dresser= ['socks' , 'shirts' , 'pants']
+
+
+say_hi('Urvita');
+
+function say_hi(name) {
+      alert('Hello ' + name)
+}
+
